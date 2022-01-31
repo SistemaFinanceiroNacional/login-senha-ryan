affc7b9259c00c3a871099bb29698e1cb714faeb
ryanjorgeac <95293822+ryanjorgeac@users.noreply.github.com>
2022-01-31 20:02:50 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+login = input("Informe o usuário: ")
+senha = input("Informe a senha: ")
+if login == "joao" and senha == "abc123":
+    print("Você está logado.")
+
+else:
+    print("Usuário negado.")
