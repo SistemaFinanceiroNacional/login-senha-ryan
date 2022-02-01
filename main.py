6d6cead5e4fc8c9bf98123589dae213573f7005a
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-01-31 21:36:25 -0300

    hashlib has been added and used on the following lines: line 7; line 8; line 10.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import getpass
+import hashlib
 
 login = input("Informe o usuário: ")
 senha = getpass.getpass("Informe a senha: ")
 
-if login == "joao" and senha == "abc123":
+hash_senha = hashlib.sha512(senha.encode("utf-8"))
+righthash = hashlib.sha512("abc123".encode("utf-8"))
+
+if login == "pedro" and hash_senha.digest() == righthash.digest():
     print("Você está logado.")
 
 else:
