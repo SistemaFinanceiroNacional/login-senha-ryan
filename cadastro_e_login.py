6a4ba8f1593947b57b67a785a2ce2cca037420f1
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-03-14 21:18:50 -0300

    .

--- cadastro_e_login.py
+++ cadastro_e_login.py
@@ -1,5 +1,6 @@
-import getpass
-import hashlib
+import contas
+import inputIO
+import password
 
 def repl(userIO,acc):
     comando = ""
@@ -13,115 +14,20 @@ def main(contas,userIO):
     choose = userIO.input("Deseja fazer login (1) ou cadastro (2)? ")
     if choose == "1":
         login = userIO.input("Informe o usuário: ")
-        password = userIO.inputoccult("Informe a senha: ")
-        possible_account = contas.authentication(login,password)
+        senha = password.password(userIO.inputoccult("Informe a senha: "))
+        possible_account = contas.authentication(login,senha)
         possible_account.map(lambda acc: repl(userIO,acc)).orElse(lambda : userIO.print("Você não está logado!"))
 
 
     elif choose == "2":
         login = userIO.input("Informe o seu novo usuário: ")
-        password = userIO.inputoccult("Informe a sua nova senha: ")
-        if contas.add_account(login,password):
+        senha = password.password(userIO.inputoccult("Informe a sua nova senha: "))
+        if contas.add_account(login,senha):
             userIO.print("Conta criada com sucesso!")
         else:
             userIO.print("Conta já existe. Tente outro usuário e senha.")
-            pass
-
-
-class contas():
-    def __init__(self,file=open("contas.txt","r+")):
-        self.archive = file
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.archive.close()
-
-    def find_login(self,login):
-        self.archive.seek(0)
-        for line in self.archive:
-            line = line[:-1]
-            logindoc,passworddoc,saldo = line.split(":")
-            if login == logindoc:
-                return (logindoc,passworddoc,saldo)
-        return ()
-
-    def add_account(self,new_login,new_password):
-        x = self.find_login(new_login)
-        new_password = self.hashpassword(new_password)
-        fstr = f"{new_login}:{new_password}\n"
-        if not x:
-            self.archive.seek(0,2)
-            self.archive.write(fstr)
-        return not x
-
-    def hashpassword(self,password):
-        hash_password = hashlib.sha512(password.encode("utf-8"))
-        return hash_password.hexdigest()
-
-    def authentication(self,login,password):
-        password = self.hashpassword(password)
-        findLoginValue = self.find_login(login)
-        if findLoginValue:
-            if password == findLoginValue[1]:
-                return just(account(findLoginValue[0],findLoginValue[1],findLoginValue[2]))
-        return nothing()
-
-
-class account():
-    def __init__(self,login,senha,saldo):
-        self.m_login = login
-        self.m_senha = senha
-        self.m_saldo = saldo
-
-    def saldo(self):
-        return self.m_saldo
-
-    def __str__(self):
-        x = f"login = {self.m_login}\nsenha = {self.m_senha}\nsaldo = {self.m_saldo}\n"
-        return x
-
-class maybe():
-    def map(self,function):
-        raise NotImplementedError
-
-    def orElse(self,default):
-        raise NotImplementedError
-
-class just(maybe):
-    def __init__(self,value):
-        self.value = value
-
-    def map(self,function):
-        return just(function(self.value))
-
-    def orElse(self,default):
-        return self.value
-
-
-class nothing(maybe):
-
-    def map(self,function):
-        return self
-
-    def orElse(self,default):
-        return default()
-
-
-class inputIO():
-    def input(self,prompt):
-        return input(prompt)
-
-    def inputoccult(self,prompt):
-        return getpass.getpass(prompt)
-
-    def print(self,prompt):
-        print(prompt)
-
-
 
 
 if __name__ == "__main__":
-    with contas() as c:
-        main(c,inputIO())
+    with contas.contas() as c:
+        main(c,inputIO.inputIO())
--- test_cadastro_e_login.py
+++ test_cadastro_e_login.py
@@ -1,6 +1,9 @@
-import io
 import cadastro_e_login
-
+import maybe
+import account
+import contas
+import filemock
+import password
 
 class inputfake():
     def __init__(self,lista):
@@ -22,29 +25,30 @@ class contasfake():
         self.actualAccounts = actualAccounts
         self.newAccounts = newAccounts
 
-    def authentication(self,login,password):
-        return login in self.actualAccounts and self.actualAccounts[login] == password
+
+    def authentication(self,login,user_password):
+        hashsenha = password.password(self.actualAccounts[login][0])
+        if login in self.actualAccounts and str(hashsenha) == str(user_password):
+            return maybe.just(account.account(login,user_password,self.actualAccounts[login][1]))
+        else:
+            return maybe.nothing()
 
     def add_account(self,new_login,new_password):
-        return new_login in self.newAccounts and self.newAccounts[new_login] == new_password
+        hashsenha = password.password(self.newAccounts[new_login])
+        return new_login in self.newAccounts and str(hashsenha) == str(new_password)
 
 def conta_do_pedro_existente():
-    return contasfake(actualAccounts={"pedro":"abc123"})
+    return contasfake(actualAccounts={"pedro":("abc123","400")})
 
 def conta_esperando_pedro():
     return contasfake(newAccounts={"pedro":"abc123"})
 
-def empty_archive():
-    return io.StringIO("")
-
-def archive_with_pedro_and_his_password():
-    return io.StringIO("pedro:eaf2c12742cb8c161bcbd84b032b9bb98999a23282542672ca01cc6edd268f7dce9987ad6b2bc79305634f89d90b90102bcd59a57e7135b8e3ceb93c0597117b\n")
 
 def test_main_with_repl():
     c = conta_do_pedro_existente()
     i = inputfake(["sair","saldo","abc123","pedro","1"])
     cadastro_e_login.main(c,i)
-    assert i.outputlist[0] == "0"
+    assert i.outputlist[0] == "400"
 
 def test_main_choose_2_already_exist():
     i = inputfake(["abc123","pedro","2"])
@@ -52,68 +56,28 @@ def test_main_choose_2_already_exist():
     cadastro_e_login.main(c,i)
     assert i.outputlist[0] == "Conta criada com sucesso!"
 
-def test_add_account():
-    archive = empty_archive()
-    new_login = "new_login"
-    new_password = "new_password"
-    x = cadastro_e_login.contas(archive)
-    y = x.add_account(new_login,new_password)
-    assert y
-
-def test_add_existing_account():
-    archive = io.StringIO("new_login:8f9afb58880507ec875fd077ed76abd4eeeff81e615648d8b8b00376d076b6e5eac0f98420341f61b07cb13f7540ae52c906014fd4412dcf56aeae8cfea2c005\n")
-    new_login = "new_login"
-    new_password = "new_password"
-    x = cadastro_e_login.contas(archive)
-    y = x.add_account(new_login, new_password)
-    assert not y
-
-def test_find_login():
-    archive = io.StringIO("ask:ask\na:a\nb:b\nc:c\n")
-    login = "a"
-    x = cadastro_e_login.contas(archive).find_login(login)
-    assert x
-
-def test_hashpassword():
-    x = cadastro_e_login.contas()
-    password = "1"
-    y = x.hashpassword(password)
-    assert y == "4dff4ea340f0a823f15d3f4f01ab62eae0e5da579ccb851f8db9dfe84c58b2b37b89903a740e1ee172da793a6e79d560e5f7f9bd058a12a280433ed6fa46510a"
-
-def test_authentication():
-    archive = io.StringIO("initial:eaf2c12742cb8c161bcbd84b032b9bb98999a23282542672ca01cc6edd268f7dce9987ad6b2bc79305634f89d90b90102bcd59a57e7135b8e3ceb93c0597117b\n")
-    l = "initial"
-    s = "text"
-    x = cadastro_e_login.contas(archive).authentication(l,s)
-    assert x == True
 
 def test_verify_correct_content():
-    archive = empty_archive()
+    archive = filemock.empty_archive()
     y = inputfake(["abc123","pedro","2"])
-    x = cadastro_e_login.contas(archive)
+    x = contas.contas(archive)
     cadastro_e_login.main(x,y)
-    assert archive.getvalue() == "pedro:c70b5dd9ebfb6f51d09d4132b7170c9d20750a7852f00680f65658f0310e810056e6763c34c9a00b0e940076f54495c169fc2302cceb312039271c43469507dc\n"
+    assert archive.getvalue() == "pedro:c70b5dd9ebfb6f51d09d4132b7170c9d20750a7852f00680f65658f0310e810056e6763c34c9a00b0e940076f54495c169fc2302cceb312039271c43469507dc:0\n"
 
 def test_verify_correct_content_using_different_password():
-    archive = archive_with_pedro_and_his_password()
+    archive = filemock.archive_with_pedro_and_his_password()
     y = inputfake(["abc123","pedro","2"])
-    x = cadastro_e_login.contas(archive)
+    x = contas.contas(archive)
     cadastro_e_login.main(x,y)
     assert y.outputlist[0] == "Conta já existe. Tente outro usuário e senha."
 
 def test_verify_content_from_last_test():
-    archive = archive_with_pedro_and_his_password()
+    archive = filemock.archive_with_pedro_and_his_password()
     y = inputfake(["abc123","pedro","1"])
-    x = cadastro_e_login.contas(archive)
+    x = contas.contas(archive)
     cadastro_e_login.main(x, y)
-    assert archive.getvalue() == "pedro:eaf2c12742cb8c161bcbd84b032b9bb98999a23282542672ca01cc6edd268f7dce9987ad6b2bc79305634f89d90b90102bcd59a57e7135b8e3ceb93c0597117b\n"
+    assert archive.getvalue() == "pedro:eaf2c12742cb8c161bcbd84b032b9bb98999a23282542672ca01cc6edd268f7dce9987ad6b2bc79305634f89d90b90102bcd59a57e7135b8e3ceb93c0597117b:40\n"
 
-def test_authentication_on_second_account():
-    archive = io.StringIO("ryan:35ca097a8dab8ede4d632f41c909a3516a259e3b954f55b081f76e627d8a85cb81e91504a8fafc25e3a9074657550f6649029dca4b8c4253a67254b57b04131c\npedro:c70b5dd9ebfb6f51d09d4132b7170c9d20750a7852f00680f65658f0310e810056e6763c34c9a00b0e940076f54495c169fc2302cceb312039271c43469507dc\n")
-    c = cadastro_e_login.contas(archive).authentication("pedro","abc123")
-    assert c
+if __name__ == "__main__":
+    test_main_with_repl()
 
-def test_authentication_on_second_account_with_real_archive():
-    with open("testbug.txt","r") as archive:
-        c = cadastro_e_login.contas(archive).authentication("pedro","abc123")
-        assert c
