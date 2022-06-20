a228e1da4e77bc9935f136a4fa9d185f19052f02
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-06-20 20:32:53 -0300

    Now the method 'head' works on multiple headers, but not working for two head calls yet.

--- Web/httpRequest.py
+++ Web/httpRequest.py
@@ -12,19 +12,30 @@ class httpRequest:
         ValueOfHeaderWithSpace = 4
         ValueOfHeader = 5
         ValueOfHeaderWithCarriageReturn = 6
-        FinalState = 7
+        NewNameOfHeader = 7
+        MaybeFinalState = 9
+        FinalState = 10
         state = FirstLine
+
+        headerName = b''
+        headerValue = b''
 
         while state != FinalState:
             nextByte = self.socket.recv(1)
-            headerName = b''
-            headerValue = b''
+            print(nextByte)
+            print(state)
+
+            if nextByte == b'':
+                raise 42
 
             if nextByte == b'\r' and state == FirstLine:
                 state = FirstLineWithCarriageReturn
 
             elif nextByte == b'\n' and state == FirstLineWithCarriageReturn:
                 state = NameOfHeader
+
+            elif nextByte == b'\r' and state == NameOfHeader:
+                state = FinalState
 
             elif nextByte != b':' and state == NameOfHeader:
                 headerName += nextByte
@@ -48,7 +59,20 @@ class httpRequest:
                 state = ValueOfHeaderWithCarriageReturn
 
             elif nextByte == b'\n' and state == ValueOfHeaderWithCarriageReturn:
+                state = NewNameOfHeader
+                header[headerName] = headerValue
+                headerName = b''
+                headerValue = b''
+
+            elif nextByte == b'\r' and state == NewNameOfHeader:
+                state = MaybeFinalState
+
+            elif nextByte == b'\n' and state == MaybeFinalState:
                 state = FinalState
 
+            elif nextByte != b':' and state == NewNameOfHeader:
+                headerName += nextByte
+                state = NameOfHeader
 
         return header
+
