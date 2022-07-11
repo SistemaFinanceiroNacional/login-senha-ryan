2ad40e745af823dc7b85c2f43c62842515880c71
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-07-11 20:48:44 -0300

    Argparse was implemented. Now, the user can fill arguments such as dbname, user, password, host, port, folder and choose between up or down.

--- migrate.py
+++ migrate.py
@@ -3,33 +3,27 @@
 import sys
 import inputIO
 import argparse
+import psycopg2
 
 def main(args, userIO):
-    i = 1
-    while i < len(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--folder", nargs=1, dest="folder", default="migrations")
+    parser.add_argument("action", choices=["up", "down"])
+    parser.add_argument("-d", "--dbname", nargs=1, default="postgres")
+    parser.add_argument("-u", "--user", nargs=1, default="postgres")
+    parser.add_argument("--password", nargs=1)
+    parser.add_argument("--host", nargs=1, default="localhost")
+    parser.add_argument("-p", "--port", nargs=1, default="5432")
+    args = parser.parse_args()
 
-        if args[i] == "--folder":
-            if i + 1 < len(args):
-                if args[i + 1].startswith("--"):
-                    userIO.print("Empty Folder")
-                    return (1)
+    if not args.password:
+        args.password = userIO.inputoccult("password:")
 
-                else:
-                    userIO.print(f"folder : {args[i + 1]}")
-
-            else:
-                userIO.print("Empty Folder")
-                return (2)
-
-        elif args[i] == "up":
-            userIO.print("up")
-            return (0)
-
-        elif args[i] == "down":
-            userIO.print("down")
-            return (0)
-
-        i += 1
+    with psycopg2.connect(f"dbname={args.dbname[0]} user={args.user[0]} password={args.password} host={args.host} port={args.port}") as conn, conn.cursor() as cursor:
+        cursor.execute("CREATE TABLE atesttable (login text, password text, balance int)")
+        cursor.execute("INSERT INTO atesttable VALUES (%s, %s, %s)", ("loginTest", "passwordTest", 100))
+        cursor.execute("SELECT * FROM atesttable")
+        print(cursor.fetchone())
 
 
 if __name__ == "__main__":
