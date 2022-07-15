3c4e95cdd33a2de17fad903981c6ad830043db50
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-07-15 19:38:23 -0300

    Two functions were implemented to help the 'up' user action. 'Down' user actions needs to be done.

--- migrate.py
+++ migrate.py
@@ -16,22 +16,39 @@ def main(args, userIO):
     parser.add_argument("--host", nargs=1, default="localhost")
     parser.add_argument("-p", "--port", nargs=1, default="5432")
     args = parser.parse_args()
-
+    print(args)
     if not args.password:
         args.password = userIO.inputoccult("password:")
 
-    p = Path(f'{args.folder}')
-    list(p.glob(f'*_{args.action}.sql'))
+    p = Path(f'{args.folder[0]}')
+    folderKids = list(p.glob(f'*_{args.action}.sql'))
 
     with psycopg2.connect(f"dbname={args.dbname[0]} user={args.user[0]} password={args.password} host={args.host} port={args.port}") as conn, conn.cursor() as cursor:
         cursor.execute("CREATE TABLE IF NOT EXISTS migrations_applied(id INT NOT NULL PRIMARY KEY, version CHAR(8)")
-        cursor.execute("CREATE TABLE atesttable (login text, password text, balance int)")
-        cursor.execute("INSERT INTO atesttable VALUES (%s, %s, %s)", ("loginTest", "passwordTest", 100))
-        cursor.execute("SELECT * FROM atesttable")
-        print(cursor.fetchone())
+        cursor.execute("SELECT version FROM migrations_applied ORDER BY version asc")
+        comparableList = cursor.fetchall()
+        if args.action == "up":
+            m = unappliedMigrations(folderKids, comparableList)
+            actionExecute(cursor, m)
+
+        elif args.action == "down":
+            pass
 
 
 
+def unappliedMigrations(allMigrationsOfAction, allMigrationsApplied):
+    unappliedMigrationsList = []
+    for i in allMigrationsApplied:
+        if i in allMigrationsOfAction:
+            unappliedMigrationsList.append(i)
+
+    return unappliedMigrationsList
+
+
+def actionExecute(cursor, migrationsToUse):
+    for i in migrationsToUse:
+        with migrationsToUse[i].open() as file:
+            cursor.execute(file.read())
 
 
 if __name__ == "__main__":
