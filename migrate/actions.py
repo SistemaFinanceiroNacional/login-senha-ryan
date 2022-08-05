80778911ad79972936bcc8e68bb753ec5218cc37
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-08-05 20:11:04 -0300

    Select on migrate.py has been moved. For statement on unappliedMigrations from actions must compare object version from object name and allMigrationsApplied list.

--- migrate/actions.py
+++ migrate/actions.py
@@ -2,8 +2,9 @@
 
 def unappliedMigrations(allMigrationsFromArgsFolder, allMigrationsApplied):
     unappliedMigrationsList = []
+    allMigrationsFromArgsFolder.sort()
     for i in allMigrationsFromArgsFolder:
-        if i not in allMigrationsApplied:
+        if i.name[0:9] not in allMigrationsApplied:
             unappliedMigrationsList.append(i)
 
     return unappliedMigrationsList
@@ -12,3 +13,9 @@ def actionExecute(cursor, migrationsToUse):
     for i in migrationsToUse:
         with i.open() as file:
             cursor.execute(file.read())
+
+def appliedMigrations(allMigrationsFromArgsFolder, allMigrationsApplied):
+    appliedMigrationsList = []
+    for i in allMigrationsApplied:
+        if i in allMigrationsFromArgsFolder:
+            appliedMigrationsList.append(i)
