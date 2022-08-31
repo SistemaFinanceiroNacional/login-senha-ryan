517e28177a4cfa8d274bc3c2b86f960efae901fe
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-08-31 20:02:28 -0300

    Types from action's methods have been changed. We did create a cursor using Protocol and it's now a type used on actionExecute method from actions. UnappliedMigrations is now splitting in the right way and veryfing only the version from a migration.

--- migrate/test_actions.py
+++ migrate/test_actions.py
@@ -1,32 +1,12 @@
 import actions
-import filemockFormigrations
 import pathlib
 
 class fakeCursor:
     def __init__(self):
         self.outputList = []
 
-    def execute(self, string):
+    def execute(self, string: str) -> None:
         self.outputList.append(string)
-
-
-def test_unappliedMigrations_1():
-    fMigrations = ["file1.sql", "file2.sql", "file3.sql", "file4.sql", "file5.sql", "file6.sql", "file7.sql"]
-    DBMigrations = ["file1.sql", "file2.sql", "file3.sql", "file4.sql", "file5.sql", "file6.sql"]
-    notUsedMigrations = actions.unappliedMigrations(fMigrations, DBMigrations)
-    assert notUsedMigrations == ["file7.sql"]
-
-def test_unappliedMigrations_2():
-    fMigrations = ["file1.sql", "file2.sql", "file3.sql", "file4.sql", "file5.sql", "file6.sql", "file7.sql"]
-    DBMigrations = ["file1.sql", "file2.sql", "file3.sql", "file4.sql", "file5.sql", "file6.sql", "file7.sql"]
-    notUsedMigrations = actions.unappliedMigrations(fMigrations, DBMigrations)
-    assert notUsedMigrations == []
-
-def test_unappliedMigrations_3():
-    fMigrations = ["file1.sql", "file2.sql", "file3.sql", "file4.sql", "file5.sql", "file6.sql", "file7.sql", "file8.sql"]
-    DBMigrations = ["file1.sql", "file2.sql", "file3.sql", "file4.sql", "file5.sql", "file6.sql"]
-    notUsedMigrations = actions.unappliedMigrations(fMigrations, DBMigrations)
-    assert notUsedMigrations == ["file7.sql", "file8.sql"]
 
 def test_actionExecute_1():
     migrationsToUsePrint = "before File and rewrite."
@@ -39,15 +19,14 @@ def test_actionExecute_1():
 def test_appliedMigrations_1():
     fMigrations = [pathlib.Path("20220708_update_account_table_up.sql"), pathlib.Path("20220612_insert_values_into_account_table_up.sql"), pathlib.Path("20220512_create_account_table_up.sql")]
     DBMigrations = [pathlib.Path("20220512_create_account_table_up.sql"), pathlib.Path("20220708_update_account_table_up.sql")]
-    downMigrations = actions.appliedMigrations(fMigrations,DBMigrations)
+    downMigrations = actions.appliedMigrations(fMigrations, DBMigrations)
     assert downMigrations == [pathlib.Path("20220512_drop_account_table_down.sql"), pathlib.Path("20220708_do_not_update_account_table_down.sql")]
 
 def test_unappliedMigrations_4():
     fMigrations = [pathlib.Path("20220708_update_account_table_up.sql")]
-    DBMigrations = [pathlib.Path("20220708_update_account_table_up.sql")]
+    DBMigrations = ["20220708_update_account_table_up.sql"]
     notUsedMigrations = actions.unappliedMigrations(fMigrations, DBMigrations)
-    assert notUsedMigrations == pathlib.Path("20220708")
-
+    assert notUsedMigrations[0] == pathlib.Path("20220708_update_account_table_up.sql")
 
 
 if __name__ == "__main__":
