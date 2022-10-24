17b30b282a7832898dafbeb576fe9c9069957783
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-10-24 19:48:23 -0300

    Only orthographic errors correction.

--- accounts.py
+++ accounts.py
@@ -1,7 +1,7 @@
 import maybe
 import internalAccount
 
-class accounts():
+class accounts:
     def __init__(self,archiveCursor):
         self.archive = archiveCursor
 
@@ -11,22 +11,21 @@ class accounts():
     def __exit__(self, exc_type, exc_val, exc_tb):
         pass
 
-    def add_account(self,new_login,new_password):
-        self.archive.execute("SELECT * FROM account WHERE login = %s ",(new_login,))
+    def add_account(self, new_login, new_password):
+        self.archive.execute("SELECT * FROM account WHERE login = %s ", (new_login,))
         x = self.archive.fetchone()
 
-
         if not x:
-            self.archive.execute("INSERT INTO account (login,password,balance) VALUES (%s,%s,%s)", (new_login,str(new_password),0))
+            self.archive.execute("INSERT INTO account (login,password,balance) VALUES (%s,%s,%s)", (new_login, str(new_password), 0))
 
         return not x
 
-    def authentication(self,login,password):
+    def authentication(self, login, password):
         self.archive.execute("SELECT * FROM account WHERE login=%s AND password=%s", (login, str(password)))
         findLoginList = self.archive.fetchone()
         if findLoginList is not None:
             return maybe.just(internalAccount.internalAccount(findLoginList[1], findLoginList[2], findLoginList[3]))
         return maybe.nothing()
 
-    def updateBalance(self,login,new_balance):
-        self.archive.execute("UPDATE account SET balance=%s WHERE login=%s",(new_balance,login))
+    def updateBalance(self, login, new_balance):
+        self.archive.execute("UPDATE account SET balance=%s WHERE login=%s", (new_balance, login))
