fcdb087d76e42a40d3f2724be4a878fcd9cbd030
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-10-24 19:49:43 -0300

    Docker Compose has been created. Actions now have one more function that works on down migrations. Its tests have been implemented on test_actions.py

--- migrate/actions.py
+++ migrate/actions.py
@@ -24,3 +24,15 @@ def appliedMigrations(allMigrationsFromArgsFolder, allMigrationsApplied):
         if i in allMigrationsFromArgsFolder:
             appliedMigrationsList.append(i)
     return appliedMigrationsList
+
+def downMigrations(allMigrationsFromArgsFolder, allMigrationsApplied):
+    downMigrationsList = []
+    for i in allMigrationsApplied:
+        iSplited = i.name.split("_")
+        for j in allMigrationsFromArgsFolder:
+            jSplited = j.name.split("_")
+            action = jSplited[-1].split(".")
+            if jSplited[0] == iSplited[0] and action[0] == "down":
+                downMigrationsList.append(j)
+
+    return downMigrationsList
