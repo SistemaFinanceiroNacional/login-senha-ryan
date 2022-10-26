6148e94beef4320a56aca7f3c40c9b1a8039f870
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-10-26 20:24:26 -0300

    Executes statements now have a right table name (account changed to accounts).

--- accounts.py
+++ accounts.py
@@ -12,20 +12,20 @@ class accounts:
         pass
 
     def add_account(self, new_login, new_password):
-        self.archive.execute("SELECT * FROM account WHERE login = %s ", (new_login,))
+        self.archive.execute("SELECT * FROM accounts WHERE login = %s ", (new_login,))
         x = self.archive.fetchone()
 
         if not x:
-            self.archive.execute("INSERT INTO account (login,password,balance) VALUES (%s,%s,%s)", (new_login, str(new_password), 0))
+            self.archive.execute("INSERT INTO accounts (login,password,balance) VALUES (%s,%s,%s)", (new_login, str(new_password), 0))
 
         return not x
 
     def authentication(self, login, password):
-        self.archive.execute("SELECT * FROM account WHERE login=%s AND password=%s", (login, str(password)))
+        self.archive.execute("SELECT login, password, balance FROM accounts WHERE login=%s AND password=%s", (login, str(password)))
         findLoginList = self.archive.fetchone()
         if findLoginList is not None:
-            return maybe.just(internalAccount.internalAccount(findLoginList[1], findLoginList[2], findLoginList[3]))
+            return maybe.just(internalAccount.internalAccount(findLoginList[0], findLoginList[1], findLoginList[2]))
         return maybe.nothing()
 
     def updateBalance(self, login, new_balance):
-        self.archive.execute("UPDATE account SET balance=%s WHERE login=%s", (new_balance, login))
+        self.archive.execute("UPDATE accounts SET balance=%s WHERE login=%s", (new_balance, login))
