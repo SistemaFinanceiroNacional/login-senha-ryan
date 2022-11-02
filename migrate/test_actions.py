17c00f5817ee4076577e8af6a5f0ef1990411e23
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-11-02 20:29:48 -0300

    UnappliedMigrations tests implemented for helping to correct the function.

--- migrate/test_actions.py
+++ migrate/test_actions.py
@@ -30,9 +30,9 @@ def test_appliedMigrations_2():
 
 def test_unappliedMigrations_4():
     fMigrations = [pathlib.Path("20220708_update_account_table_up.sql")]
-    DBMigrations = ["20220708_update_account_table_up.sql"]
+    DBMigrations = ["20220708"]
     notUsedMigrations = actions.unappliedMigrations(fMigrations, DBMigrations)
-    assert notUsedMigrations[0] == pathlib.Path("20220708_update_account_table_up.sql")
+    assert notUsedMigrations == []
 
 def test_downMigrations_1():
     appliedMigrations = [pathlib.Path("20220708_update_account_table_up.sql"), pathlib.Path("20220709_create_service_table_up.sql")]
@@ -44,8 +44,33 @@ def test_downMigrations_2():
     appliedMigrations = [pathlib.Path("20220708_update_account_table_up.sql"), pathlib.Path("20220709_create_service_table_up.sql")]
     fMigrations = [pathlib.Path("20220708_update_account_table_up.sql"), pathlib.Path("20220709_create_service_table_up.sql"), pathlib.Path("20220709_drop_service_table_down.sql")]
     downMigrations = actions.downMigrations(fMigrations, appliedMigrations)
-    assert downMigrations == [ pathlib.Path("20220709_drop_service_table_down.sql")]
+    assert downMigrations == [pathlib.Path("20220709_drop_service_table_down.sql")]
+
+
+def test_version_unappliedMigrations():
+    fMigrations = [pathlib.Path("20220708_update_account_table_up.sql"),pathlib.Path("20220810_update_account_tablee_up.sql"),pathlib.Path("20220920_update_account_table_up.sql"), pathlib.Path("20221008_update_account_table_up.sql")]
+    DBMigrations = ["20220708"]
+    notUsedMigrations = actions.unappliedMigrations(fMigrations, DBMigrations)
+    assert len(notUsedMigrations) == len(fMigrations) - 1
+
+def test_version_unappliedMigrations1():
+    fMigrations = [pathlib.Path("20220708_update_account_table_up.sql"),pathlib.Path("20220810_update_account_tablee_up.sql"),pathlib.Path("20220920_update_account_table_up.sql"), pathlib.Path("20221008_update_account_table_up.sql")]
+    DBMigrations = ["20221008"]
+    notUsedMigrations = actions.unappliedMigrations(fMigrations, DBMigrations)
+    assert len(notUsedMigrations) == 0
+
+def test_version_unappliedMigrations_empty_list():
+    fMigrations = [pathlib.Path("20220708_update_account_table_up.sql"),pathlib.Path("20220810_update_account_tablee_up.sql"), pathlib.Path("20220920_update_account_table_up.sql"), pathlib.Path("20221008_update_account_table_up.sql")]
+    DBMigrations = []
+    notUsedMigrations = actions.unappliedMigrations(fMigrations, DBMigrations)
+    assert len(notUsedMigrations) == 4
+
+def test_version_unappliedMigrations_emptyString_on_list():
+    fMigrations = [pathlib.Path("20220708_update_account_table_up.sql"),pathlib.Path("20220810_update_account_tablee_up.sql"), pathlib.Path("20220920_update_account_table_up.sql"), pathlib.Path("20221008_update_account_table_up.sql")]
+    DBMigrations = [""]
+    notUsedMigrations = actions.unappliedMigrations(fMigrations, DBMigrations)
+    assert len(notUsedMigrations) == 4
 
 
 if __name__ == "__main__":
-    test_actionExecute_1()
+    test_unappliedMigrations_4()
