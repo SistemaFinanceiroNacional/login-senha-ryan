0f6441a0b32ce4ff89960ab1f9ea70e459539014
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-11-02 20:26:11 -0300

    Migrations don't insert into migrations' version control table, this job it's on migrate.py. One migration has been created for testing.

--- migrations/20220704_drop_account_table_down.sql
+++ migrations/20220704_drop_account_table_down.sql
@@ -1,2 +1,1 @@
 DROP TABLE accounts;
-DELETE FROM migrations_applied WHERE version='20220704';
--- migrations/20221030_insert_into_accounts_joao_account_up.sql
+++ migrations/20221030_insert_into_accounts_joao_account_up.sql
@@ -1,2 +1,1 @@
 INSERT INTO accounts (login, password) VALUES ('joao', 'ab123');
-INSERT INTO migrations_applied (version) VALUES ('20221030');
