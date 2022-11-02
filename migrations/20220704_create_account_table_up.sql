73102e3fa88f334aa6ddffa6ad8fafeed1f451a0
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-11-02 20:26:11 -0300

    Migrations don't insert into migrations' version control table, this job it's on migrate.py. One migration has been created for testing.

--- migrations/20220704_create_account_table_up.sql
+++ migrations/20220704_create_account_table_up.sql
@@ -1,4 +1,2 @@
 CREATE TABLE IF NOT EXISTS accounts (id INT GENERATED ALWAYS
 AS IDENTITY PRIMARY KEY, login text NOT NULL, password text NOT NULL, balance INT DEFAULT 0);
-CREATE TABLE IF NOT EXISTS migrations_applied(id SERIAL PRIMARY KEY, version CHAR(8));
-INSERT INTO migrations_applied (version) VALUES ('20220704');
