c1601a72d5d56ec15aa22cb6272afa09673876e4
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-11-16 20:39:32 -0300

    It's time to work on httpResponse. We've implemented a function to return a response as bytes named 'responseAsBytes'. Some tests have been implemented as well.

--- Web/httpResponse.py
+++ Web/httpResponse.py
@@ -1,5 +1,30 @@
 
-
 class httpResponse:
-    def asbytes(self):
-        pass
+    def __init__(self, headers, body, status):
+        self.headers = headers
+        self.body = body
+        self.status = status
+
+    def getHeaders(self):
+        return self.headers
+
+    def getBody(self):
+        return self.body
+
+    def getStatus(self):
+        return self.status
+
+
+def responseAsBytes(response):
+    headers = response.getHeaders()
+    body = response.getBody()
+    headers["Content-length"] = len(body)
+    status = response.getStatus()
+    version = "1.1"
+    responseToBytes = f"HTTP/{version} {status} OK\r\n"
+    for key in headers:
+        responseToBytes += f"{key}: {headers[key]}\r\n"
+
+    responseToBytes += "\r\n"+f"{body}"
+
+    return responseToBytes.encode("utf-8")
---
+++ Web/test_httpResponse.py
@@ -0,0 +1,27 @@
+import pytest
+
+from Web import httpResponse
+
+@pytest.fixture
+def responseExample():
+    headers = {"Date": "Mon, 23 May 2005 22:38:34 GMT"}
+    body = "<?xml version='1.0' encoding='UTF-8'?>"
+    status = "200"
+    return httpResponse.httpResponse(headers, body, status)
+
+def test_httpResponse_1(responseExample):
+    assert responseExample.getStatus() == "200"
+
+def test_httpResponse_get_body(responseExample):
+    assert responseExample.getBody() == "<?xml version='1.0' encoding='UTF-8'?>"
+
+def test_httpResponse_responseAsBytes_Body(responseExample):
+    responseAsBytes = httpResponse.responseAsBytes(responseExample).split(b"\r\n\r\n")
+    body = responseAsBytes[1]
+    assert body == b"<?xml version='1.0' encoding='UTF-8'?>"
+
+def test_httpResponse_responseAsBytes_Headers(responseExample):
+    responseAsBytes = httpResponse.responseAsBytes(responseExample).split(b"\r\n\r\n")
+    headers = responseAsBytes[0]
+    setFromHeader = {*headers.split(b"\r\n")}
+    assert setFromHeader == {b"HTTP/1.1 200 OK", b"Date: Mon, 23 May 2005 22:38:34 GMT", b"Content-length: 38"}
