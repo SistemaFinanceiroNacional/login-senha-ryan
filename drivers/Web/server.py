704cd7a66f105ea743678c9d814cf5350669fa9b
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2023-01-13 19:25:52 -0300

    with statement removed because its object has no close.

--- drivers/Web/server.py
+++ drivers/Web/server.py
@@ -12,11 +12,10 @@ def main():
         serverSocket.bind(("127.0.0.1", 8080))
         serverSocket.listen()
         logging.info("serverSocket listened")
-        with serverSocket.accept() as connectionSocket:
-            clientSocket, addr = connectionSocket
-            logging.debug(f"Client connected on IP {clientSocket.getpeername()}")
-            connection = httpConnection.httpConnection(clientSocket)
-            connection.process(bankApplication.root)
+        clientSocket, addr = serverSocket.accept()
+        logging.debug(f"Client connected on IP {clientSocket.getpeername()}")
+        connection = httpConnection.httpConnection(clientSocket)
+        connection.process(bankApplication.root)
 
 
 if __name__ == "__main__":
