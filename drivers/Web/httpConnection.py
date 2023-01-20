6e1cf859a98fd7dad6fbbe67f9037b86963e2adb
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2023-01-20 20:16:28 -0300

    Enable user activate a log instead of a package.

--- drivers/Web/httpConnection.py
+++ drivers/Web/httpConnection.py
@@ -3,8 +3,7 @@ from drivers.Web import httpRequest, httpResponse
 
 import logging
 
-logger = logging.getLogger(__name__)
-
+logger = logging.getLogger("drivers.Web.httpConnection")
 
 class httpConnection:
     def __init__(self, socket):
@@ -13,6 +12,7 @@ class httpConnection:
     def process(self, handler):
         while True:
             request = httpRequest.getNextHttpRequest(self.socket)
+            logger.info(f"Resource: {request.getResource()}; Method: {request.getMethod()}")
             response = handler(request)
             self.socket.sendall(httpResponse.responseAsBytes(response))
             if request.getHeaders().get('Connection', '') == "close":
