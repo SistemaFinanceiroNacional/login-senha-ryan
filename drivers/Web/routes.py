e6aa3ee947eebec0d3c1fccbe53b9eb60270bd78
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2023-01-30 19:29:18 -0300

    With method_dispatcher on routes being the base class of handlers helps to remove if's conditionals once every handler overwrittes the methods of a http verb.

--- drivers/Web/routes.py
+++ drivers/Web/routes.py
@@ -1,4 +1,5 @@
 import maybe
+from drivers.Web import httpResponse
 
 class endpointRoute:
     def __init__(self, endpoint, handler):
@@ -20,3 +21,32 @@ class fixed_route:
         if request.getResource().getEndpoint() == self.fixed_url:
             return maybe.just(self.handler(request))
         return maybe.nothing()
+
+class method_dispatcher:
+    def __call__(self, request):
+        method = request.getMethod().lower()
+        attribute = getattr(type(self), method, None)
+        class_attribute = getattr(method_dispatcher, method, None)
+        if attribute != class_attribute:
+            return attribute(request)
+
+        else:
+            return httpResponse.httpResponse({"Allow": self.allowed_methods()}, "", 405)
+
+    def allowed_methods(self):
+        possible_methods = ["get", "post"]
+        allowed_methods_list = []
+        for method in possible_methods:
+            if getattr(type(self), method) != getattr(method_dispatcher, method):
+                allowed_methods_list.append(method.upper())
+
+        return ", ".join(allowed_methods_list)
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+    def head(self, request):
+        pass
