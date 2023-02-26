840c72d88bb7a822b1488b2eb2af1a355b473378
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2023-02-26 09:57:08 -0300

    We have a auxiliar lexer to use on parser. Parser's statements have their classes and their own render method. Also, pytest coverage can be useful :)

--- drivers/Web/template.py
+++ drivers/Web/template.py
@@ -1,13 +1,21 @@
 import os.path
 
+from drivers.Web.lexer import lexer, auxiliar_lexer
+import drivers.Web.parser as parser
+
 templates_path = os.path.dirname(__file__) + "/templates/"
 
-def render_template(template_name, context):
+def render_template(template_name, context, file_lexer=None):
+    lexer_of_file = file_lexer or file_system_file_lexer
+    alexer = lexer_of_file(template_name)
+    return parser.parser(alexer, lexer_of_file).render(context, {})
+
+
+def file_system_file_lexer(template_name):
     file_name = templates_path + template_name
     with open(file_name) as file:
         file_content = file.read()
 
-    for key, value in context.items():
-        file_content = file_content.replace(f"{{{key}}}", value)
+    lxer = lexer(file_content)
+    return auxiliar_lexer(lxer)
 
-    return file_content
