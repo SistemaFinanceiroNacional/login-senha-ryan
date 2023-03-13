cb68cffc9ad4ebb8bc28e9e5c32a8611e392030f
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2023-03-13 20:01:35 -0300

    Refund working correctly. The get_connection from a connection_pool is now treating cases of trying to reach a connection, but lock needed when the amount of connections is on the maximum allowed.

--- ApplicationService/connection_pool.py
+++ ApplicationService/connection_pool.py
@@ -11,29 +11,48 @@ class connection_pool:
     def get_cursor(self, identifier: identity):
         raise NotImplementedError()
 
+    def refund(self, identifier: identity):
+        raise NotImplementedError()
+
 class connection:
     def cursor(self):
         raise NotImplementedError
 
 
 class postgresql_connection_pool(connection_pool):
-    def __init__(self, createConnection=None):
-        self.createConnection = createConnection or psycopg2_create_connection
+    def __init__(self, createConnection=None, max_connections=1):
+        self.create_connection = createConnection or psycopg2_create_connection
         self.connections: dict[int, tuple] = dict()  # tuple[0] are connections and tuple[1] are conn's cursor
+        self.max_connections = max_connections
+        self.free_connections = []
 
     def get_connection(self, identifier: identity):
-        if identifier.value() not in self.connections.keys():
-            conn = self.createConnection()
-            self.connections[identifier.value()] = (conn, conn.cursor())
+        id_conn = identifier.value()
+        if id_conn not in self.connections.keys():
+            if self.free_connections:
+                conn = self.free_connections.pop()
+                self.connections[id_conn] = (conn, conn.cursor())
 
-        return self.connections[identifier.value()][0]
+            elif len(self.connections) < self.max_connections:
+                conn = self.create_connection()
+                self.connections[id_conn] = (conn, conn.cursor())
+
+            else:
+                pass
+
+        return self.connections[id_conn][0]
 
     def get_cursor(self, identifier: identity):
         if identifier.value() not in self.connections.keys():
-            conn = self.createConnection()
+            conn = self.create_connection()
             self.connections[identifier.value()] = (conn, conn.cursor())
 
         return self.connections[identifier.value()][1]
+
+    def refund(self, identifier: identity):
+        if identifier.value() in self.connections.keys():
+            conn = self.connections.pop(identifier.value())[0]
+            self.free_connections.append(conn)
 
 
 def psycopg2_create_connection():
--- test_command_line_interface.py
+++ test_command_line_interface.py
@@ -7,7 +7,7 @@ import password
 import psycopg2
 
 class inputfake:
-    def __init__(self,lista):
+    def __init__(self, lista):
         self.inputlist = lista
         self.outputlist = []
 
@@ -22,11 +22,11 @@ class inputfake:
 
 
 class contasfake:
-    def __init__(self,actualAccounts,newAccounts):
+    def __init__(self,actualAccounts, newAccounts):
         self.actualAccounts = actualAccounts
         self.newAccounts = newAccounts
 
-    def authentication(self,login,user_password):
+    def authentication(self, login, user_password):
         hashsenha = password.password(self.actualAccounts[login][0])
         if login in self.actualAccounts and str(hashsenha) == str(user_password):
             return maybe.just(internalAccount.internalAccount(login, user_password, self.actualAccounts[login][1]))
@@ -39,28 +39,28 @@ class contasfake:
 
 
 def existing_pedros_account():
-    return contasfake(actualAccounts={"pedro":("abc123","400")}, newAccounts={})
+    return contasfake(actualAccounts={"pedro": ("abc123", "400")}, newAccounts={})
 
 def waiting_pedro_account():
-    return contasfake(newAccounts={"pedro":"abc123"}, actualAccounts={})
+    return contasfake(newAccounts={"pedro": "abc123"}, actualAccounts={})
 
 
 def test_main_with_repl():
     conn = psycopg2.connect("dbname=test user=ryanbanco password=abc123 host=localhost")
     cursor = conn.cursor()
     c = existing_pedros_account()
-    i = inputfake(["logout","balance","abc123","pedro","1"])
+    i = inputfake(["logout", "balance", "abc123", "pedro", "1"])
     z = externalAccountsInteractions.externalAccountsInteractions(cursor)
-    login_and_registration.main(c, z, i)
+    command_line_interface.main(c, z, i)
     assert i.outputlist[0] == "400"
 
 def test_main_choose_2_already_exist():
     conn = psycopg2.connect("dbname=test user=ryanbanco password=abc123 host=localhost")
     cursor = conn.cursor()
-    i = inputfake(["abc123","pedro","2"])
+    i = inputfake(["abc123", "pedro", "2"])
     c = waiting_pedro_account()
     z = externalAccountsInteractions.externalAccountsInteractions(cursor)
-    login_and_registration.main(c, z, i)
+    command_line_interface.main(c, z, i)
     assert i.outputlist[0] == "Your account has been successfully created!"
 
 def test_verify_correct_content_using_different_password():
@@ -70,10 +70,10 @@ def test_verify_correct_content_using_different_password():
     x = accounts.accounts(cursor)
     new_login = "pedro"
     new_password = password.password("ab123")
-    x.add_account(new_login,new_password)
-    y = inputfake(["abc123","pedro","2"])
+    x.add_account(new_login, new_password)
+    y = inputfake(["abc123", "pedro", "2"])
     z = externalAccountsInteractions.externalAccountsInteractions(cursor)
-    login_and_registration.main(x, z, y)
+    command_line_interface.main(x, z, y)
     cursor.close()
     conn.rollback()
     conn.close()
