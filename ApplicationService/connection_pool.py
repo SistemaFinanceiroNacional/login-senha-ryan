45314d22914dbb5e6e2433125bee1f454f814a37
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2023-04-20 18:54:55 -0300

    URL corrected. Post and get from open_account_handler implemented.

--- ApplicationService/connection_pool.py
+++ ApplicationService/connection_pool.py
@@ -7,14 +7,10 @@ from ApplicationService.repositories.connectionInterface import cursor, connecti
 
 
 class postgresql_connection_pool(connection_pool):
-    createConnections: Callable
-    max_connection: int
-    condition: Condition
-
     def __init__(self, createConnection: Callable[[], connection] = None, max_connections: int = 1, condition=None):
-        self.create_connection: Callable[[], connection] = createConnection or psycopg2_create_connection
+        self.create_connection = createConnection or psycopg2_create_connection
         self.allocated_connections: dict[int, tuple[connection, cursor]] = dict()
-        self.max_connections: int = max_connections
+        self.max_connections = max_connections
         self.free_connections: list[tuple[connection, cursor]] = []
         self.condition = condition() if condition else Condition()
 
