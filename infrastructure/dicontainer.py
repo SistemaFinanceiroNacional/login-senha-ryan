d041b374948ab05412a068b76168a5749947e451
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2023-07-26 15:00:16 -0300

    dicontainer now has parameters and tests.

--- infrastructure/dicontainer.py
+++ infrastructure/dicontainer.py
@@ -4,6 +4,7 @@ import inspect
 class DiContainer:
     def __init__(self):
         self.objects = dict()
+        self.parameters = dict()
 
     def __getitem__(self, item):
         if item not in self.objects:
@@ -13,12 +14,17 @@ class DiContainer:
     def __setitem__(self, key, value):
         self.objects[key] = value
 
+    def set_parameter(self, key, value):
+        self.parameters[key] = value
+
     def construct_obj(self, item):
         sign = inspect.signature(item)
         params = sign.parameters
         dict_params = {}
-        for param_name in params:
-            param = params[param_name]
-            dict_params[param.name] = self[param.annotation]
+        for param_name, param_annotation in params.items():
+            if param_name in self.parameters:
+                dict_params[param_name] = self.parameters[param_name]
+            else:
+                dict_params[param_name] = self[param_annotation]
 
         return item(**dict_params)
